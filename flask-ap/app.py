5cb66ceb62d3fd6e3ed70c1a4161c1f6e5690c12
c10t <c10t@fa.ke>
2018-04-13 06:18:28 +0900

    add template

--- flask-ap/app.py
+++ flask-ap/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis, RedisError
 import os
 import socket
@@ -15,10 +15,8 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return render_template('hello.html',
+                           name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
